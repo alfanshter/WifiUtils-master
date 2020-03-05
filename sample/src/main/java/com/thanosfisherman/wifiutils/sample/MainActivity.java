17683460bb12d20b58767524e80f95e00cf29d3e
alfanshter <alfanno8@gmail.com>
2020-03-05 16:44:22 +0700

    Initial commit

---
+++ sample/src/main/java/com/thanosfisherman/wifiutils/sample/MainActivity.java
@@ -0,0 +1,55 @@
+package com.thanosfisherman.wifiutils.sample;
+
+import android.Manifest;
+import android.os.Build;
+import android.os.Bundle;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.thanosfisherman.wifiutils.WifiUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 555);
+        final Button button = findViewById(R.id.button);
+        WifiUtils.enableLog(true);
+        //TODO: CHECK IF LOCATION SERVICES ARE ON
+        button.setOnClickListener(v -> connectWithWpa());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void connectWithWps() {
+        WifiUtils.withContext(getApplicationContext()).connectWithWps("d8:74:95:e6:f5:f8", "51362485").onConnectionWpsResult(this::checkResult).start();
+    }
+
+    private void connectWithWpa() {
+        String ote = "Novian";
+        String otePass = "00000000";
+
+        WifiUtils.withContext(getApplicationContext())
+                .connectWith(ote, otePass)
+                .setTimeout(40000)
+                .onConnectionResult(this::checkResult)
+                .start();
+    }
+
+    private void enableWifi() {
+        WifiUtils.withContext(getApplicationContext()).enableWifi(this::checkResult);
+        //or without the callback
+        //WifiUtils.withContext(getApplicationContext()).enableWifi();
+    }
+
+    private void checkResult(boolean isSuccess) {
+        if (isSuccess)
+            Toast.makeText(MainActivity.this, "SUCCESS!", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(MainActivity.this, "EPIC FAIL!", Toast.LENGTH_SHORT).show();
+    }
+}
